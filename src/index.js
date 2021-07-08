42264ca1b284a67f23694e369ff9ed755c1e23ed
rr21pepe <pepe@redradix.com>
2021-07-09 01:14:42 +0200

    Dome but I want to try a little refactor to iterate through the commands

---
+++ src/index.js
@@ -0,0 +1,148 @@
+const DIRECTIONS = {
+  N: 'N',
+  S: 'S',
+  E: 'E',
+  W: 'W'
+}
+
+const COMMANDS = {
+  r: 'r',
+  l: 'l',
+  f: 'f',
+  b: 'b'
+}
+
+const turnRight = direction => {
+  const nextDirection = {
+    [DIRECTIONS.N]: DIRECTIONS.E,
+    [DIRECTIONS.E]: DIRECTIONS.S,
+    [DIRECTIONS.S]: DIRECTIONS.W,
+    [DIRECTIONS.W]: DIRECTIONS.N
+  }
+
+  return nextDirection[direction]
+}
+
+const turnLeft = direction => {
+  const nextDirection = {
+    [DIRECTIONS.N]: DIRECTIONS.W,
+    [DIRECTIONS.W]: DIRECTIONS.S,
+    [DIRECTIONS.S]: DIRECTIONS.E,
+    [DIRECTIONS.E]: DIRECTIONS.N
+  }
+
+  return nextDirection[direction]
+}
+
+const turn = (direction, command) => command === COMMANDS.l
+  ? turnLeft(direction)
+  : turnRight(direction)
+
+const moveForwardsOrBackwards = (position, direction, command) => {
+  const _direction = command === COMMANDS.f ? 1 : -1
+  const nextCell = {
+    [DIRECTIONS.N]: ([row, col]) => [row - _direction, col],
+    [DIRECTIONS.E]: ([row, col]) => [row, col + _direction],
+    [DIRECTIONS.S]: ([row, col]) => [row + _direction, col],
+    [DIRECTIONS.W]: ([row, col]) => [row, col - _direction]
+  }
+
+  return nextCell[direction](position)
+}
+
+const lastElement = arr => arr[arr.length - 1]
+
+const isValidCommand = command => COMMANDS.hasOwnProperty(command)
+
+const isArray = arr => Array.isArray(arr)
+
+const isChar = str => typeof str === 'string' && str.length === 1
+
+const isPositiveCoord = coord => coord >= 0
+
+const isContainedInGrid = (coord, gridSize) => {
+  const [rows, cols] = gridSize
+  const [row, col] = coord
+
+  return (row < rows && col < cols) && (row >= 0 && col >= 0)
+}
+
+const validateStartingPoint = (start, gridSize) => {
+  if(!isArray(start)) return false
+  if(!start.every(isPositiveCoord)) return false
+  if(!isContainedInGrid(start, gridSize)) return false
+
+  return true
+}
+
+const validateDirection = direction => DIRECTIONS.hasOwnProperty(direction)
+
+const validateGridSize = gridSize => {
+  if(!isArray(gridSize)) return false
+  if(!gridSize.every(isPositiveCoord)) return false
+
+  return true
+}
+
+const validateCommands = commands => {
+  if(!isArray(commands)) return false
+  if(!commands.every(isChar)) return false
+  if(!commands.every(isValidCommand)) return false
+
+  return true
+}
+
+const validateParams = (start, direction, gridSize, commands) => {
+  const isValidStart = validateStartingPoint(start, gridSize)
+  const isValidDirection = validateDirection(direction)
+  const isValidGridSize = validateGridSize(gridSize)
+  const isValidCommandSequence = validateCommands(commands)
+
+  return [
+    isValidStart,
+    isValidDirection,
+    isValidGridSize,
+    isValidCommandSequence
+  ].every(validated => validated)
+}
+
+const getNextPosition = (position, direction, command) => {
+  let nextPosition = [...position]
+  switch(command) {
+    case COMMANDS.f:
+    case COMMANDS.b:
+      nextPosition = moveForwardsOrBackwards(position, direction, command); break
+  }
+
+  return nextPosition
+}
+
+const executeCommands = (start, startDirection, gridSize, commands) => {
+  if(!validateParams(start, startDirection, gridSize, commands)) {
+    return []
+  }
+
+  let direction = startDirection
+  let position = start
+
+  const trace = commands.map(command => {
+    direction = [COMMANDS.r, COMMANDS.l].includes(command)
+      ? turn(direction, command)
+      : direction
+    const nextPosition = getNextPosition(position, direction, command)
+
+    if(isContainedInGrid(nextPosition, gridSize)) {
+      position = nextPosition
+    }
+
+    return position
+  })
+
+  return lastElement(trace)
+}
+
+module.exports = {
+  executeCommands,
+  DIRECTIONS,
+  COMMANDS
+}
--- src/index.test.js
+++ src/index.test.js
@@ -1,3 +1,72 @@
-describe('Mars Rovers', () => {
+const { executeCommands, DIRECTIONS } = require('./index')
 
+const { E, N, S, W } = DIRECTIONS
+
+describe('Mars Rovers', () => {
+  describe('Validating starting point parameter', () => {
+    it('Should return [] when starting point is not correct (1)', () => {
+      expect(executeCommands([-1, 0], N, [3, 3], ['f', 'r'])).toEqual([])
+    })
+    it('Should return [] when starting point is not correct (2)', () => {
+      expect(executeCommands([0, -1], N, [3, 3], ['f', 'r'])).toEqual([])
+    })
+    it('Should return [] when starting point is not correct (3)', () => {
+      expect(executeCommands('[0, -1]', N, [3, 3], ['f', 'r'])).toEqual([])
+    })
+    it('Should return [] when starting point is not correct (4)', () => {
+      expect(executeCommands(8, N, [3, 3], ['f', 'r'])).toEqual([])
+    })
+    it('Should return [] when starting point is bigger than grid size (1)', () => {
+      expect(executeCommands([5, 2], E, [3, 3], ['f', 'r'])).toEqual([])
+    })
+    it('Should return [] when starting point is bigger than grid size (2)', () => {
+      expect(executeCommands([2, 5], E, [3, 3], ['f', 'r'])).toEqual([])
+    })
+  })
+
+  describe('Validating direction parameter', () => {
+    it('Should return [] when direction is not correct (1)', () => {
+      expect(executeCommands([1, 0], 'T', [3, 3], ['f', 'r'])).toEqual([])
+    });
+    it('Should return [] when direction is not correct (2)', () => {
+      expect(executeCommands([1, 0], 3, [3, 3], ['f', 'r'])).toEqual([])
+    });
+  })
+
+  describe('Validating grid size parameter', () => {
+    it('Should return [] when grid size is not correct (1)', () => {
+      expect(executeCommands([1, 5], W, [-1, 0], ['f', 'r'])).toEqual([])
+    });
+    it('Should return [] when grid size is not correct (2)', () => {
+      expect(executeCommands([1, 5], E, [0, -1], ['f', 'r'])).toEqual([])
+    });
+    it('Should return [] when grid size is not correct (3)', () => {
+      expect(executeCommands([1, 5], E, '[0, -1]', ['f', 'r'])).toEqual([])
+    });
+  })
+
+  describe('Validating commands parameter', () => {
+    it('Should return [] when commands sequence is not correct (1)', () => {
+      expect(executeCommands([0, 3], N, [4, 4], ['ff', 'r', 'l'])).toEqual([])
+    })
+    it('Should return [] when commands sequence is not correct (2)', () => {
+      expect(executeCommands([0, 3], N, [4, 4], ['ffrflbrf'])).toEqual([])
+    })
+    it('Should return [] when commands sequence is not correct (3)', () => {
+      expect(executeCommands([0, 3], N, [4, 4], ['f', 'f', 'd', 'r'])).toEqual([])
+    })
+  })
+
+  it('Should return [0, 4] when: starting: [2, 4]; direction: N; commands: flflfrfrffrff; grid size: [5, 6]', () => {
+    const commands = ["f", "l", "f", "l", "f", "r", "f", "r", "f", "f", "r", "f", "f"]
+    expect(executeCommands([2, 4], N, [5, 6], commands)).toEqual([0, 4])
+  })
+  it('Should return [1, 1] when: starting: [4, 0]; direction: E; commands: lfflfrrflflfb; grid size: [5, 6]', () => {
+    const commands = ["l", "f", "f", "l", "f", "r", "r", "f", "l", "f", "l", "f", "b"]
+    expect(executeCommands([4, 0], E, [5, 6], commands)).toEqual([1, 1])
+  })
+  it('Should return [4, 4] when: starting: [5, 6]; direction: N; commands: bfrbbblblbb; grid size: [5, 6]', () => {
+    const commands = ["b", "f", "r", "b", "b", "b", "l", "b", "l", "b", "b"]
+    expect(executeCommands([4, 5], N, [5, 6], commands)).toEqual([4, 4])
+  })
 })
